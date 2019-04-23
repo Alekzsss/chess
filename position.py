16cf1229576b6fda71152277c94017b272da6c1f
Alekzsss <alekzsss@gmail.com>
2019-04-23 18:16:39 +0300

    second commit

--- position.py
+++ position.py
@@ -1,6 +1,20 @@
+from itertools import cycle
+
+
 class Position:
-    def __init__(self, coordinates, color):
-        self.x = coordinates[0]
-        self.y = coordinates[1]
-        self.color = color
+
+    _colors = cycle(['black', 'white'])
+
+    def __init__(self, position):
+        self.x = position[0]
+        self.y = position[1]
+        self.color = next(self._colors)
         self.resident = None
+
+if __name__ == '__main__':
+    p = Position("d1")
+    print("\n".join([p.x, p.y, str(p.color), str(p.resident)]))
+    p2 = Position("d2")
+    print("\n".join([p2.x, p2.y, str(p2.color), str(p2.resident)]))
+    p3 = Position("d3")
+    print("\n".join([p3.x, p3.y, str(p3.color), str(p3.resident)]))
